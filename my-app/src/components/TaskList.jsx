106729b2dea2d01e0d9e48da4cf7c035478a3e40
NoeliaAlvarez <noelia.ag1990@gmail.com>
2022-10-07 13:25:29 +0200

    useContext

--- my-app/src/components/TaskList.jsx
+++ my-app/src/components/TaskList.jsx
@@ -1,5 +1,9 @@
 import TaskCard from "./TaskCard";
-export function TaskList({tasks, deleteTask}) {
+import { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
+
+function TaskList() {
+  const {tasks} = useContext(TaskContext);
 
   if(tasks.length === 0) {
     return <h1>No hay tareas</h1>
@@ -8,9 +12,10 @@ export function TaskList({tasks, deleteTask}) {
   return (
     <div>
       {tasks.map((task) => (
-        <TaskCard key={task.id} task={task} deleteTask={deleteTask}/>
+        <TaskCard key={task.id} task={task}/>
       ))}
     </div>
   );
 }
 
+export default TaskList;
